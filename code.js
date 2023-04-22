ac1598443cdec5301684320cbdb41ff11dbaa03f
uttiya <uttiyasarkar@gmail.com>
2023-04-22 08:54:03 +0530

    new webpage

--- code.js
+++ code.js
@@ -1,239 +1,101 @@
-var rows = 38;
-var cols = 100;
+/*jslint browser:true */
+$(document).ready(function () {
+	var $body = $('body');
+	var $navbar = $('.navbar-default');
+	var $offsetY = $navbar.offset().top + 10;
+	var $menuButton = $('button.navbar-toggle');
+	var $menuIcon = $('.navbar-toggle .glyphicon');
+	var $collapsedMenuItem = $('.navbar-collapse.collapse li');
+	var $modalBackdropDiv = $('<div class="modal-backdrop fade in"></div>');
+	var $scrollButton = $('.scroll');
+	var $socialIcon = $('.social');
 
-var playing = false;
+	// Fixed Nav after scroll
+	function scroll() {
+		if ($(window).scrollTop() >= $offsetY) {
+			$navbar.addClass('menu-fixed').css('background-color', 'rgba(255,254,253,0.97)');
+		} else {
+			$navbar.removeClass('menu-fixed').css('background-color', 'transparent');
+		}
+	}
+	document.onscroll = scroll;
 
-var grid = new Array(rows);
-var nextGrid = new Array(rows);
+	// Mobile Menu functions
+	function openMenu() {
+		$menuIcon.removeClass('glyphicon-menu-hamburger').addClass('glyphicon-remove active');
+		$modalBackdropDiv.css('z-index', 900);
+		$body.append($modalBackdropDiv);
+		if (!$navbar.hasClass('menu-fixed')) {
+			$navbar.css('background-color', 'rgba(255,254,253,0.97)');
+		}
+		// Close menu after clicking modal-backdrop
+		$modalBackdropDiv.on('click', function () {
+			$('.navbar-toggle').click();
+			closeMenu();
+		});
+	}
+	function closeMenu() {
+		$menuIcon.removeClass('glyphicon-remove active').addClass('glyphicon-menu-hamburger');
+		$modalBackdropDiv.css('z-index', 1025).remove();
+		if (!$navbar.hasClass('menu-fixed')) {
+			$navbar.css('background-color', 'transparent');
+		}
+	}
+	// Mobile Menu Icon Toggle
+	$menuButton.on('click', function () {
+		if ($menuIcon.hasClass('glyphicon-menu-hamburger')) {
+			openMenu();
+			// Close menu after clicking a link
+			$collapsedMenuItem.on('click', function () {
+				$('.navbar-toggle').click(); // Trigger collapse animation
+				closeMenu();
+			});
+		} else {
+			closeMenu();
+		}
+	});
+	// Collapse menu on resize
+	$(window).resize(closeMenu());
 
-var timer;
-var reproductionTime = 100;
+	// Smooth scroll to content
+	$scrollButton.on('click', function (e) {
+		e.preventDefault();
+		var $link = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $($link).offset().top - 60
+		}, 900);
+	});
 
-function initializeGrids() {
-    for (var i = 0; i < rows; i++) {
-        grid[i] = new Array(cols);
-        nextGrid[i] = new Array(cols);
-    }
-}
+	// Social icons hover effect
+	$socialIcon.on({
+		'focus mouseenter': function () {
+			var $iconImg = $(this).children();
+			var $href = $iconImg.attr('src').slice(0, -18) + 'color.png?raw=true'; // Remove 'black.svg' from end and add 'color.svg'
+			$iconImg.attr('src', $href);
+		},
+		'blur mouseleave': function () {
+			var $iconImg = $(this).children();
+			var $href = $iconImg.attr('src').slice(0, -18) + 'black.png?raw=true';
+			$iconImg.attr('src', $href);
+		}
+	});
 
-function resetGrids() {
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < cols; j++) {
-            grid[i][j] = 0;
-            nextGrid[i][j] = 0;
-        }
-    }
-}
+	// Center modals vertically
+	function centerModal() {
+    $(this).css('display', 'block');
+    var $dialog = $(this).find('.modal-dialog');
+    var $offset = ($(window).height() - $dialog.height()) / 2;
+    var $bottomMargin = parseInt($dialog.css('margin-bottom'), 10);
 
-function copyAndResetGrid() {
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < cols; j++) {
-            grid[i][j] = nextGrid[i][j];
-            nextGrid[i][j] = 0;
-        }
+    // If modal is taller than screen height, top margin = bottom margin
+    if ($offset < $bottomMargin) {
+    	$offset = $bottomMargin;
     }
-}
+    $dialog.css('margin-top', $offset);
+  }
 
-// Initialize
-function initialize() {
-    createTable();
-    initializeGrids();
-    resetGrids();
-    setupControlButtons();
-}
-
-// Lay out the board
-function createTable() {
-    var gridContainer = document.getElementById('gridContainer');
-    if (!gridContainer) {
-        // Throw error
-        console.error("Problem: No div for the drid table!");
-    }
-    var table = document.createElement("table");
-    
-    for (var i = 0; i < rows; i++) {
-        var tr = document.createElement("tr");
-        for (var j = 0; j < cols; j++) {//
-            var cell = document.createElement("td");
-            cell.setAttribute("id", i + "_" + j);
-            cell.setAttribute("class", "dead");
-            cell.onclick = cellClickHandler;
-            tr.appendChild(cell);
-        }
-        table.appendChild(tr);
-    }
-    gridContainer.appendChild(table);
-    }
-
-    function cellClickHandler() {
-        var rowcol = this.id.split("_");
-        var row = rowcol[0];
-        var col = rowcol[1];
-        
-        var classes = this.getAttribute("class");
-        if(classes.indexOf("live") > -1) {
-            this.setAttribute("class", "dead");
-            grid[row][col] = 0;
-        } else {
-            this.setAttribute("class", "live");
-            grid[row][col] = 1;
-        }
-        
-    }
-
-    function updateView() {
-        for (var i = 0; i < rows; i++) {
-            for (var j = 0; j < cols; j++) {
-                var cell = document.getElementById(i + "_" + j);
-                if (grid[i][j] == 0) {
-                    cell.setAttribute("class", "dead");
-                } else {
-                    cell.setAttribute("class", "live");
-                }
-            }
-        }
-    }
-
-function setupControlButtons() {
-    // button to start
-    var startButton = document.getElementById('start');
-    startButton.onclick = startButtonHandler;
-    
-    // button to clear
-    var clearButton = document.getElementById('clear');
-    clearButton.onclick = clearButtonHandler;
-    
-    // button to set random initial state
-    var randomButton = document.getElementById("random");
-    randomButton.onclick = randomButtonHandler;
-}
-
-function randomButtonHandler() {
-    if (playing) return;
-    clearButtonHandler();
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < cols; j++) {
-            var isLive = Math.round(Math.random());
-            if (isLive == 1) {
-                var cell = document.getElementById(i + "_" + j);
-                cell.setAttribute("class", "live");
-                grid[i][j] = 1;
-            }
-        }
-    }
-}
-
-// clear the grid
-function clearButtonHandler() {
-    console.log("Clear the game: stop playing, clear the grid");
-    
-    playing = false;
-    var startButton = document.getElementById('start');
-    startButton.innerHTML = "Start";    
-    clearTimeout(timer);
-    
-    var cellsList = document.getElementsByClassName("live");
-    // convert to array first, otherwise, you're working on a live node list
-    // and the update doesn't work!
-    var cells = [];
-    for (var i = 0; i < cellsList.length; i++) {
-        cells.push(cellsList[i]);
-    }
-    
-    for (var i = 0; i < cells.length; i++) {
-        cells[i].setAttribute("class", "dead");
-    }
-    resetGrids;
-}
-
-// start/pause/continue the game
-function startButtonHandler() {
-    if (playing) {
-        console.log("Pause the game");
-        playing = false;
-        this.innerHTML = "Continue";
-        clearTimeout(timer);
-    } else {
-        console.log("Continue the game");
-        playing = true;
-        this.innerHTML = "Pause";
-        play();
-    }
-}
-
-// run the life game
-function play() {
-    computeNextGen();
-    
-    if (playing) {
-        timer = setTimeout(play, reproductionTime);
-    }
-}
-
-function computeNextGen() {
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < cols; j++) {
-            applyRules(i, j);
-        }
-    }
-    
-    // copy NextGrid to grid, and reset nextGrid
-    copyAndResetGrid();
-    // copy all 1 values to "live" in the table
-    updateView();
-}
-
-// RULES
-// Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-// Any live cell with two or three live neighbours lives on to the next generation.
-// Any live cell with more than three live neighbours dies, as if by overcrowding.
-// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
-function applyRules(row, col) {
-    var numNeighbors = countNeighbors(row, col);
-    if (grid[row][col] == 1) {
-        if (numNeighbors < 2) {
-            nextGrid[row][col] = 0;
-        } else if (numNeighbors == 2 || numNeighbors == 3) {
-            nextGrid[row][col] = 1;
-        } else if (numNeighbors > 3) {
-            nextGrid[row][col] = 0;
-        }
-    } else if (grid[row][col] == 0) {
-            if (numNeighbors == 3) {
-                nextGrid[row][col] = 1;
-            }
-        }
-    }
-    
-function countNeighbors(row, col) {
-    var count = 0;
-    if (row-1 >= 0) {
-        if (grid[row-1][col] == 1) count++;
-    }
-    if (row-1 >= 0 && col-1 >= 0) {
-        if (grid[row-1][col-1] == 1) count++;
-    }
-    if (row-1 >= 0 && col+1 < cols) {
-        if (grid[row-1][col+1] == 1) count++;
-    }
-    if (col-1 >= 0) {
-        if (grid[row][col-1] == 1) count++;
-    }
-    if (col+1 < cols) {
-        if (grid[row][col+1] == 1) count++;
-    }
-    if (row+1 < rows) {
-        if (grid[row+1][col] == 1) count++;
-    }
-    if (row+1 < rows && col-1 >= 0) {
-        if (grid[row+1][col-1] == 1) count++;
-    }
-    if (row+1 < rows && col+1 < cols) {
-        if (grid[row+1][col+1] == 1) count++;
-    }
-    return count;
-}
-
-// Start everything
-window.onload = initialize;
+  $(document).on('show.bs.modal', '.modal', centerModal);
+  $(window).on('resize', function () {
+    $('.modal:visible').each(centerModal);
+  });
+});
